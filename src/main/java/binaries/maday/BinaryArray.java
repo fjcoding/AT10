76c882e58640bc51cf74e16a61ddb35ae665ce11
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-24 19:06:46 -0400

    Solved comments
    Changed spanish to english
    Meaningful names
    Adding spaces

--- src/main/java/binaries/maday/BinaryArray.java
+++ src/main/java/binaries/maday/BinaryArray.java
@@ -2,30 +2,30 @@ package binaries.maday;
 
 public class BinaryArray {
 
-    /**
-     * Array of 1s and 0s expected
-     */ 
-    private int[] binaries;
+	/**
+	 * Array of 1s and 0s expected
+	 */
+	private int[] binaries;
 
-    /**
-     * Simple constructor that assigns binaries list to instace's state
-     */
-    public BinaryArray(int[] binaries) {
-        this.binaries = binaries;
-    }
+	/**
+	 * Simple constructor that assigns binaries list to instace's state
+	 */
+	public BinaryArray(int[] binaries) {
+		this.binaries = binaries;
+	}
 
-    /**
-     * Returns the decimal numeric value equivalent to the list of binaries stored
-     */
-    public int ConvertToDecimalInt() {
-        //throw new UnsupportedOperationException("Not supported yet.");
-        int potencia = binaries.length-1;
-		int decimal = 0;		
-		for (int i : binaries) {
-			int convertidos= i*(int)(Math.pow(2, potencia));
-			decimal = decimal + convertidos;
-			potencia--;
+	/**
+	 * Returns the decimal numeric value equivalent to the list of binaries stored
+	 */
+	public int ConvertToDecimalInt() {
+		final int BASE = 2;
+		int power = binaries.length - 1;		
+		int decimal = 0;
+		for (int index : binaries) {
+			int converted = index * (int) (Math.pow(BASE, power));
+			decimal = decimal + converted;
+			power--;
 		}
 		return decimal;
-    }
+	}
 }
