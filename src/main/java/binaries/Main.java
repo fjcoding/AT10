3807b117ca594dde5f30d652e77026b4dd5131cd
Jose Guevara <jguevara@pros.com>
2019-05-24 03:49:38 -0400

    Add "Ones and Zeros" kata

---
+++ src/main/java/binaries/Main.java
@@ -0,0 +1,66 @@
+package binaries;
+
+/**
+ * Main class runner for BinaryArrau practice
+ * DO NOT TOUCH THIS CLASS! just run it ;)
+ *
+ * How to execute this class?
+ *      1. Open a command line interface (cmd, git bash, etc)
+ *      2. Navigate to root project AT10
+ *      3. Execute the following command:
+ *          [WINDOWS CMD]>   gradlew.bat run --args="<your name> <input list of binaries>"
+ *          [BASH]>          ./gradlew run --args="<your name> <input list of binaries>"
+ */
+public class Main {
+
+    static final int STUDENT = 0;
+    static final int BINARY_START = 1;
+
+    // AT10 students
+    static final String ALEJANDRO = "alejandro";
+    static final String ANDRES = "andres";
+    static final String JESUS = "jesus";
+    static final String JOHN = "john";
+    static final String JOSUE = "josue";
+    static final String LIMBERT = "limbert";
+    static final String MADAY = "maday";
+    static final String MELISSA = "melissa";
+
+    public static void main(String[] args) {
+        String student = getStudent(args);
+        int[] argsAsInts = toInts(args);
+        printResult(student, argsAsInts);
+    }
+
+    private static String getStudent(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("you forgot to specify the student... doh!");
+        } else {
+            return args[STUDENT];
+        }
+    }
+
+    private static int[] toInts(String[] args) {
+        int[] argsAsInts = new int[args.length];
+        for (int index = BINARY_START; index < args.length; index++) {
+            argsAsInts[index] = Integer.parseInt(args[index]);
+        }
+        return argsAsInts;
+    }
+
+    private static void printResult(String student, int[] argsAsInts) {
+        int result = 0;
+        switch (student) {
+            case ALEJANDRO: result = new binaries.alejandro.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case ANDRES: result = new binaries.andres.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case JESUS: result = new binaries.jesus.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case JOHN: result = new binaries.john.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case JOSUE: result = new binaries.josue.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case LIMBERT: result = new binaries.limbert.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case MADAY: result = new binaries.maday.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            case MELISSA: result = new binaries.melissa.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+            default: throw new IllegalArgumentException("Unknown student! who are you?");
+        }
+        System.out.println(student + "'s result is: " + result);
+    }
+}
---
+++ src/main/java/binaries/josue/BinaryArray.java
@@ -0,0 +1,23 @@
+package binaries.josue;
+
+public class BinaryArray {
+
+    /**
+     * Array of 1s and 0s expected
+     */ 
+    private int[] binaries;
+
+    /**
+     * Simple constructor that assigns binaries list to instace's state
+     */
+    public BinaryArray(int[] binaries) {
+        this.binaries = binaries;
+    }
+
+    /**
+     * Returns the decimal numeric value equivalent to the list of binaries stored
+     */
+    public int ConvertToDecimalInt() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
