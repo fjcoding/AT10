91931f0fb5a1d202bf70da049101791b3fa8c90e
TomDebianW <johnpiterzon@gmail.com>
2019-05-24 13:34:24 -0400

    Initial implementation

--- src/main/java/binaries/john/BinaryArray.java
+++ src/main/java/binaries/john/BinaryArray.java
@@ -18,6 +18,15 @@ public class BinaryArray {
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //throw new UnsupportedOperationException("Not supported yet.");
+    	  int exp = 0;
+          double decimal = 0;
+
+          for(int i = 0; i < binaries.length; i++) {
+              for(exp = binaries.length - 1; exp < 0; exp++) {
+                  decimal = decimal + binaries[i] * (int)Math.pow(2, binaries.length - 1);
+              }
+          }
+		return (int) decimal;
     }
 }
