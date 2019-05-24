c06807fd61e7a9f2397029ef10036a21bdd5999c
Jesus.Menacho@fundacion-jala.org <Jesus.Menacho@fundacion-jala.org>
2019-05-24 16:32:09 -0400

    I write the code for ConvertToDecimalInt

--- src/main/java/binaries/jesus/BinaryArray.java
+++ src/main/java/binaries/jesus/BinaryArray.java
@@ -18,6 +18,55 @@ public class BinaryArray {
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //throw new UnsupportedOperationException("Not supported yet.");
+        if (binaries==null) {
+            return 0;
+        }
+
+        int [] arrPow=new int [binaries.length];
+        for (int i = 0; i < binaries.length; i++) {
+            arrPow[i]= (int) pow(binaries)[i];
+        }
+        return sumPow(arrPow,binaries);
     }
+
+    public  int binary(int arrBinary[]) {
+        if (arrBinary==null) {
+            return 0;
+        }
+
+        int [] arrPow=new int [arrBinary.length];
+        for (int i = 0; i < arrBinary.length; i++) {
+            arrPow[i]= (int) pow(arrBinary)[i];
+        }
+        return sumPow(arrPow,arrBinary);
+    }
+
+    public int sumPow(int [] arrPow,int arrBinary[]){
+        int sum=0;
+        for (int i = 0; i <arrBinary.length ; i++) {
+            if (arrBinary[i]==1){
+                sum+=arrPow[i];
+            }
+        }
+        return sum;
+    }
+
+    public  double [] pow(int arrayBinary[]){
+        double [] arrPow= new double[arrayBinary.length];
+        int suma=0;
+        int position=0;
+        for (int i = arrayBinary.length-1; i >=0; i--) {
+            arrPow[position] = Math.pow(2, i);
+            position++;
+        }
+        return arrPow;
+    }
+
+    public static void main(String arg[]){
+        int [] num={1,1,1,1};
+        BinaryArray binaryArray = new BinaryArray(num);
+        System.out.println("El resultado es="+binaryArray.ConvertToDecimalInt());
+    }
+
 }
---
+++ src/test/java/binaries/jesus/BinaryArrayTest.java
@@ -0,0 +1,7 @@
+package binaries.jesus;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryArrayTest {
+
+}
