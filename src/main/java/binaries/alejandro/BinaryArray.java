af529bf6c1cb881d5121df2d6a08251879083629
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-24 17:20:40 -0400

    Suggested corrections incorporated to code

--- src/main/java/binaries/alejandro/BinaryArray.java
+++ src/main/java/binaries/alejandro/BinaryArray.java
@@ -18,10 +18,13 @@ public class BinaryArray {
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
-	    int result = 0;
-		for (int i=binaries.length-1; i>0; i--) {
-			result += (binaries[i])*(Math.pow( 2,binaries.length - 1 - i));
-		}
-		return result;
+        final int BASE = 2;
+        int result = 0;
+        int upperLimit = binaries.length - 1;
+        int lowerLimit = 0;
+        for (int arrayIndex = upperLimit; arrayIndex > lowerLimit; arrayIndex--) {
+            result += (binaries[arrayIndex]) * (Math.pow(BASE, upperLimit - arrayIndex));
+        }
+        return result;
     }
 }
