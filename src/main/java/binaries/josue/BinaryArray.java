f6f99afd02a081234f1d9243931711f4ffc72304
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-24 12:35:33 -0400

    ConvertToDecimalInt() method implemented

--- src/main/java/binaries/josue/BinaryArray.java
+++ src/main/java/binaries/josue/BinaryArray.java
@@ -2,22 +2,28 @@ package binaries.josue;
 
 public class BinaryArray {
 
-    /**
-     * Array of 1s and 0s expected
-     */ 
-    private int[] binaries;
+	/**
+	 * Array of 1s and 0s expected
+	 */
+	private int[] binaries;
 
-    /**
-     * Simple constructor that assigns binaries list to instace's state
-     */
-    public BinaryArray(int[] binaries) {
-        this.binaries = binaries;
-    }
+	/**
+	 * Simple constructor that assigns binaries list to instace's state
+	 */
+	public BinaryArray(int[] binaries) {
+		this.binaries = binaries;
+	}
 
-    /**
-     * Returns the decimal numeric value equivalent to the list of binaries stored
-     */
-    public int ConvertToDecimalInt() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	/**
+	 * Returns the decimal numeric value equivalent to the list of binaries stored
+	 */
+	public int ConvertToDecimalInt() {
+		int res = 0;
+		int cont = 0;
+		for (int i = binaries.length - 1; i > 0; i--) {
+			res = res + ((int) (Math.pow(2, cont)) * binaries[i]);
+			cont++;
+		}
+		return res;
+	}
 }
