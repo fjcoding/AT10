ce637e3866d15755227c31547b32c5959ca844c7
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-24 13:20:31 -0400

    ConvertToDecimalInt() implemented

--- src/main/java/binaries/maday/BinaryArray.java
+++ src/main/java/binaries/maday/BinaryArray.java
@@ -18,6 +18,14 @@ public class BinaryArray {
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //throw new UnsupportedOperationException("Not supported yet.");
+        int potencia = binaries.length-1;
+		int decimal = 0;		
+		for (int i : binaries) {
+			int convertidos= i*(int)(Math.pow(2, potencia));
+			decimal = decimal + convertidos;
+			potencia--;
+		}
+		return decimal;
     }
 }
