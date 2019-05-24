1646d12a99c33ff009bfb255f5beeb0ba259bffd
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-05-24 12:49:00 -0400

    Ctrl + Shift + F

--- src/main/java/binaries/melissa/BinaryArray.java
+++ src/main/java/binaries/melissa/BinaryArray.java
@@ -2,28 +2,28 @@ package binaries.melissa;
 
 public class BinaryArray {
 
-    /**
-     * Array of 1s and 0s expected
-     */ 
-    private int[] binaries;
+	/**
+	 * Array of 1s and 0s expected
+	 */
+	private int[] binaries;
 
-    /**
-     * Simple constructor that assigns binaries list to instace's state
-     */
-    public BinaryArray(int[] binaries) {
-        this.binaries = binaries;
-    }
+	/**
+	 * Simple constructor that assigns binaries list to instace's state
+	 */
+	public BinaryArray(int[] binaries) {
+		this.binaries = binaries;
+	}
 
-    /**
-     * Returns the decimal numeric value equivalent to the list of binaries stored
-     */
-    public int ConvertToDecimalInt() {
-    	int decimal = 0;
+	/**
+	 * Returns the decimal numeric value equivalent to the list of binaries stored
+	 */
+	public int ConvertToDecimalInt() {
+		int decimal = 0;
 		double powOfTwo;
-		for (int i = 0; i <binaries.length ; i++) {
-			powOfTwo = Math.pow(2, binaries.length-i-1);
+		for (int i = 0; i < binaries.length; i++) {
+			powOfTwo = Math.pow(2, binaries.length - i - 1);
 			decimal = decimal + binaries[i] * (int) powOfTwo;
 		}
 		return decimal;
-    }
+	}
 }
