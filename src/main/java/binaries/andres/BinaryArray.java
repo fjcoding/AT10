a2fcc005cf9b3eaeac387a7ceeb309c7986d3e18
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-05-24 11:24:35 -0400

    First kata solved

--- src/main/java/binaries/andres/BinaryArray.java
+++ src/main/java/binaries/andres/BinaryArray.java
@@ -2,22 +2,27 @@ package binaries.andres;
 
 public class BinaryArray {
 
-    /**
-     * Array of 1s and 0s expected
-     */ 
-    private int[] binaries;
+	/**
+	 * Array of 1s and 0s expected
+	 */
+	private int[] binaries;
 
-    /**
-     * Simple constructor that assigns binaries list to instace's state
-     */
-    public BinaryArray(int[] binaries) {
-        this.binaries = binaries;
-    }
+	/**
+	 * Simple constructor that assigns binaries list to instace's state
+	 */
+	public BinaryArray(int[] binaries) {
+		this.binaries = binaries;
+	}
 
-    /**
-     * Returns the decimal numeric value equivalent to the list of binaries stored
-     */
-    public int ConvertToDecimalInt() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	/**
+	 * Returns the decimal numeric value equivalent to the list of binaries stored
+	 */
+	public int ConvertToDecimalInt() {
+
+		int res = 0;
+		for (int i = 0; i < binaries.length; i++) {
+			res = (res * 2) + binaries[i];
+		}
+		return res;
+	}
 }
