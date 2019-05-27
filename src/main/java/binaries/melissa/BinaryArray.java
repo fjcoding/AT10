b92e33f1068911ff2590e04d6ca29bf604698b38
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-05-27 17:50:16 -0400

    Tabs replaced by 4 steps

--- src/main/java/binaries/melissa/BinaryArray.java
+++ src/main/java/binaries/melissa/BinaryArray.java
@@ -4,17 +4,17 @@ import java.lang.Math;
 
 public class BinaryArray {
 
-	/**
-	 * Array of 1s and 0s expected
-	 */
-	private int[] binaries;
+    /**
+     * Array of 1s and 0s expected
+     */
+    private int[] binaries;
 
-	/**
-	 * Simple constructor that assigns binaries list to instace's state
-	 */
-	public BinaryArray(int[] binaries) {
-		this.binaries = binaries;
-	}
+    /**
+     * Simple constructor that assigns binaries list to instace's state
+     */
+    public BinaryArray(int[] binaries) {
+        this.binaries = binaries;
+    }
 
     /**
      * Returns the decimal numeric value equivalent to the list of binaries stored
