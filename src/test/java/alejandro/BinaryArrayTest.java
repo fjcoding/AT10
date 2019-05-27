c5e12b06d3b4a25ed7e2b2cb02e521a5c698dceb
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-27 18:35:39 -0400

    Implement First Unit Test

--- src/test/java/alejandro/BinaryArrayTest.java
+++ src/test/java/alejandro/BinaryArrayTest.java
@@ -5,4 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+    @Test
+    public void ConvertToDecimalInt_noDigits_result0() {
+        BinaryArray binaryArray = new BinaryArray(new int[]{});
+        assertEquals(0, binaryArray.ConvertToDecimalInt());
+    }
 }
