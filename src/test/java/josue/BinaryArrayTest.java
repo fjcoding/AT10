6b2bd49ad65c1ad98c96257ee3c2df841fb97463
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-27 19:06:01 -0400

    unit test

--- src/test/java/josue/BinaryArrayTest.java
+++ src/test/java/josue/BinaryArrayTest.java
@@ -4,5 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import binaries.limbert.BinaryArray;
+import jdk.nashorn.internal.AssertsEnabled;
+
 public class BinaryArrayTest {
+    @Test
+    public void ConvertToDecimalInt_result43{
+        BinaryArray expeted = new BinaryArray(new int[] {1 0 1 0 1 1});
+        assertsEquals(43,expeted.ConvertToDecimalInt());
+        
+    }
 }
