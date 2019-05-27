83591204ae9b5e42391e6559e1929e5057e24ad2
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-05-27 19:16:24 -0400

    basic unit test added

--- src/test/java/andres/BinaryArrayTest.java
+++ src/test/java/andres/BinaryArrayTest.java
@@ -1,8 +1,16 @@
 package binaries.andres;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class BinaryArrayTest {
+    
+    @Test
+    public void ConvertToDecimalIntTest_1000_8() {
+        int[] binary = {1,0,0,0};
+        BinaryArray converter = new BinaryArray(binary);
+        int expected = 8;
+        int actual = converter.ConvertToDecimalInt();
+        assertEquals(actual,expected);
+    }
 }
