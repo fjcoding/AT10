e797dba51c3fa215dda38e281d79896e831f9b2f
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-27 17:33:24 -0400

    Fix the tabs and put .classpath in ignore

--- src/main/java/binaries/maday/BinaryArray.java
+++ src/main/java/binaries/maday/BinaryArray.java
@@ -2,30 +2,30 @@ package binaries.maday;
 
 public class BinaryArray {
 
-	/**
-	 * Array of 1s and 0s expected
-	 */
-	private int[] binaries;
+    /**
+     * Array of 1s and 0s expected
+     */
+    private int[] binaries;
 
-	/**
-	 * Simple constructor that assigns binaries list to instace's state
-	 */
-	public BinaryArray(int[] binaries) {
-		this.binaries = binaries;
-	}
+    /**
+     * Simple constructor that assigns binaries list to instace's state
+     */
+    public BinaryArray(int[] binaries) {
+        this.binaries = binaries;
+    }
 
-	/**
-	 * Returns the decimal numeric value equivalent to the list of binaries stored
-	 */
-	public int ConvertToDecimalInt() {
-		final int BASE = 2;
-		int power = binaries.length - 1;		
-		int decimal = 0;
-		for (int index : binaries) {
-			int converted = index * (int) (Math.pow(BASE, power));
-			decimal = decimal + converted;
-			power--;
-		}
-		return decimal;
-	}
+    /**
+     * Returns the decimal numeric value equivalent to the list of binaries stored
+     */
+    public int ConvertToDecimalInt() {
+        final int BASE = 2;
+        int power = binaries.length - 1;
+        int decimal = 0;
+        for (int index : binaries) {
+            int converted = index * (int) (Math.pow(BASE, power));
+            decimal = decimal + converted;
+            power--;
+        }
+        return decimal;
+    }
 }
