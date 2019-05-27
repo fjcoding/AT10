ef411de5b5395418cc447363d26c898eee293bac
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-27 11:57:28 -0400

    Generic names were changed to more specific names

--- src/main/java/binaries/josue/BinaryArray.java
+++ src/main/java/binaries/josue/BinaryArray.java
@@ -1,28 +1,29 @@
 package binaries.josue;
-    
-public class BinaryArray {
-	final int base = 2;
-	/**
-	 * Array of 1s and 0s expected
-	 */
-	private int[] binaries;
-	/**
-	 * Simple constructor that assigns binaries list to instace's state
-	 */
-	public BinaryArray(int[] binaries) {
-		this.binaries = binaries;
-	}
 
-	/**
-	 * Returns the decimal numeric value equivalent to the list of binaries stored
-	 */
-	public int ConvertToDecimalInt() {
-		int result = 0;
-		int cont = 0;
-		for (int index = binaries.length - 1; index > 0; index--) {
-			result = result + ((int) (Math.pow(base,cont)) * binaries[index]);
-			cont++;
-		}
-		return result;
-	}
+public class BinaryArray {
+    final int base = 2;
+    /**
+     * Array of 1s and 0s expected
+     */
+    private int[] binaries;
+    /**
+     * Simple constructor that assigns binaries list to instace's state
+     */
+    public BinaryArray(int[] binaries) {
+        this.binaries = binaries;
+    }
+
+    /**
+     * Returns the decimal numeric value equivalent to the list of binaries
+     * stored
+     */
+    public int ConvertToDecimalInt() {
+        int result = 0;
+        int cont = 0;
+        for (int index = binaries.length - 1; index > 0; index--) {
+            result = result + ((int) (Math.pow(base, cont)) * binaries[index]);
+            cont++;
+        }
+        return result;
+    }
 }
