cb8d18f46025d41ddb4890248342ed6828c64b50
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-28 11:56:16 -0400

    Add Unit Test Cases
    Cases considered:
    * No digits
    * Invalid digits
    * Single 0
    * Single 1
    * Index 0 digit is 0
    * Index 0 digit is 1

--- src/main/java/binaries/alejandro/BinaryArray.java
+++ src/main/java/binaries/alejandro/BinaryArray.java
@@ -2,6 +2,9 @@ package binaries.alejandro;
 
 public class BinaryArray {
 
+    final int BIN_BASE = 2;
+    final int HEX_BASE = 16;
+
     /**
      * Array of 1s and 0s expected
      */ 
@@ -18,13 +21,22 @@ public class BinaryArray {
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
-        final int BASE = 2;
-        int result = 0;
-        int upperLimit = binaries.length - 1;
-        int lowerLimit = 0;
-        for (int arrayIndex = upperLimit; arrayIndex > lowerLimit; arrayIndex--) {
-            result += (binaries[arrayIndex]) * (Math.pow(BASE, upperLimit - arrayIndex));
+        if (binaries.length != 0) {
+            int result = 0;
+            int upperLimit = binaries.length - 1;
+            int lowerLimit = 0;
+            for (int arrayIndex = upperLimit; arrayIndex >= lowerLimit; arrayIndex--) {
+                if ((binaries[arrayIndex] >= 0) && (binaries[arrayIndex] < BIN_BASE)) {
+                    result += (binaries[arrayIndex]) * (Math.pow(BIN_BASE, upperLimit - arrayIndex));
+                }
+                else {
+                    throw new IllegalArgumentException("Error: Invalid binary digits were entered!");
+                }
+            }
+            return result;
         }
-        return result;
+        else {
+            throw new IllegalArgumentException("Caution: No digits were entered!");
+        }
     }
 }
--- src/test/java/alejandro/BinaryArrayTest.java
+++ src/test/java/alejandro/BinaryArrayTest.java
@@ -5,9 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+    @Test (expected = IllegalArgumentException.class)
+    public void ConvertToDecimalInt_noDigits_resultIllegalArgumentException() {
+        (new BinaryArray(new int[]{})).ConvertToDecimalInt();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void ConvertToDecimalInt_invalidDigits_resultIllegalArgumentException() {
+        (new BinaryArray(new int[]{-1, 6, -9})).ConvertToDecimalInt();
+    }
+
     @Test
-    public void ConvertToDecimalInt_noDigits_result0() {
-        BinaryArray binaryArray = new BinaryArray(new int[]{});
-        assertEquals(0, binaryArray.ConvertToDecimalInt());
+    public void ConvertToDecimalInt_singleBit0_result509() {
+        int expected = 0;
+        int actual = (new BinaryArray(new int[]{0})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalInt_singleBit1_result509() {
+        int expected = 1;
+        int actual = (new BinaryArray(new int[]{1})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalInt_mostSignificativeBit0_result509() {
+        int expected = 509;
+        int actual = (new BinaryArray(new int[]{0, 1, 1, 1, 1, 1, 1, 1, 0, 1})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalInt_mostSignificativeBit1_result509() {
+        int expected = 509;
+        int actual = (new BinaryArray(new int[]{1, 1, 1, 1, 1, 1, 1, 0, 1})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
     }
 }
