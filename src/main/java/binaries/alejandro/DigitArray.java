3d273e518793fd9390802c45d4729f5344306824
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-29 17:10:43 -0400

    Add DigitArray Unit Test
    Add Mock DigitArray class
    Add DigitArrayTest for testing DigitArray class.

---
+++ src/main/java/binaries/alejandro/DigitArray.java
@@ -0,0 +1,26 @@
+package binaries.alejandro;
+
+public class DigitArray {
+
+    int base;
+
+    /**
+     * Array of 1s and 0s expected
+     */ 
+    protected int[] digits;
+
+    /**
+     * Simple constructor that assigns binaries list to instace's state
+     */
+    public DigitArray(int[] digits) {
+        this.base = 10;
+        this.digits = digits;
+    }
+
+    /**
+     * Returns the decimal numeric value equivalent to the list of binaries stored
+     */
+    public int ConvertToDecimalInt() {
+        return 0;
+    }
+}
---
+++ src/test/java/alejandro/DigitArrayTest.java
@@ -0,0 +1,14 @@
+package binaries.alejandro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryArrayTest {
+    @Test
+    public void ConvertToDecimalInt_singleBit1_result1() {
+        int expected = 1;
+        int actual = (new DigitArray(new int[]{1})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+}
