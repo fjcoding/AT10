18a647d1569447d679165fb625ef75391b61e7b3
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-05-29 17:39:35 -0400

    BinaryToDecimal and HexadecimalToDecimal UnitTest's included

--- src/main/java/binaries/andres/HexadecimalArray.java
+++ src/main/java/binaries/andres/HexadecimalArray.java
@@ -17,16 +17,7 @@ public class HexadecimalArray {
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
-        String hexadecimalsDigits="0123456789ABCDEF";
-        hexadecimals = hexadecimals.toUpperCase();
-        int result = 0;
-        for (int index = 0; index < hexadecimals.length(); index++)
-        {
-            char digit = hexadecimals.charAt(index);
-            int decimal = hexadecimalsDigits.indexOf(digit);
-            result = 16*result + decimal;
-        }
-        return result;
+        throw new UnsupportedOperationException("Not a binary digit");
         
     }
 }
