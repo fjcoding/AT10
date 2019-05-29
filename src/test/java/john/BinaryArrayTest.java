0d820d7a7aea366e2fa24d2138f6587132ff18c1
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-29 13:28:42 -0400

    changing the files from john to josue my code was writing it in the john package by mistake

--- src/test/java/john/BinaryArrayTest.java
+++ src/test/java/john/BinaryArrayTest.java
@@ -4,22 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import binaries.josue.BinaryArray;
 
 public class BinaryArrayTest {
-	@Test
-	public void convertToDecimalInt_enterEmptyString_result0() {
-		BinaryArray actual = new BinaryArray(new int[] {});
-		assertEquals(0, actual.ConvertToDecimalInt());
-	}
-	@Test
-	public void convertToDecimalInt_enterNotEmptyString_result43() {
-		BinaryArray actual = new BinaryArray(new int[] {1,0,1,0,1,1});
-		assertEquals(43, actual.ConvertToDecimalInt());
-	}
-	@Test(expected = Exception.class)
-	public void convertToDecimalInt_enterNotEmptyStringWithNumbersNotBinarie_resultNotBinarie() {
-		BinaryArray actual = new BinaryArray(new int [] {1,-1});
-		assertEquals("Not binario", actual.ConvertToDecimalInt());
-	}
 }
--- src/test/java/josue/BinaryArrayTest.java
+++ src/test/java/josue/BinaryArrayTest.java
@@ -5,4 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+	@Test
+	public void convertToDecimalInt_enterEmptyString_result0() {
+		BinaryArray actual = new BinaryArray(new int[] {});
+		assertEquals(0, actual.ConvertToDecimalInt());
+	}
+	@Test
+	public void convertToDecimalInt_enterNotEmptyString_result43() {
+		BinaryArray actual = new BinaryArray(new int[] {1,0,1,0,1,1});
+		assertEquals(43, actual.ConvertToDecimalInt());
+	}
+	@Test(expected = Exception.class)
+	public void convertToDecimalInt_enterNotEmptyStringWithNumbersNotBinarie_resultNotBinarie() {
+		BinaryArray actual = new BinaryArray(new int [] {1,-1});
+		assertEquals("Not binario", actual.ConvertToDecimalInt());
+	}
 }
