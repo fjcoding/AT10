472c94c55546b61f486c9c83c3d4a32b43407c9a
Limbert Vargas Laura <LimbertVargas@Fundacion-jala.local>
2019-05-29 13:57:49 -0400

    Adding unit test with valid data

--- src/test/java/limbert/BinaryArrayTest.java
+++ src/test/java/limbert/BinaryArrayTest.java
@@ -1,14 +1,24 @@
-package binaries.limbert;
+package limbert;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import binaries.limbert.BinaryArray;
+
 public class BinaryArrayTest {
     @Test
-    public void ConvertToDecimalInt_result43() {
+    public void ConvertToDecimalInt_resultEmpty() {
         int expected = 0;
         BinaryArray binary = new BinaryArray(new int[] { });
+        int actual = binary.ConvertToDecimalInt();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void ConvertToDecimalInt_result43() {
+        int expected = 43;
+        BinaryArray binary = new BinaryArray(new int[] {1, 0, 1, 0, 1, 1});
         int actual = binary.ConvertToDecimalInt();
         assertEquals(expected,actual);
     }
