27eda1535e17a12a9df5f112c1db14d1bd6e1436
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-29 12:06:50 -0400

    add unit-test for one not empty String

--- src/test/java/john/BinaryArrayTest.java
+++ src/test/java/john/BinaryArrayTest.java
@@ -12,4 +12,10 @@ public class BinaryArrayTest {
 		BinaryArray actual = new BinaryArray(new int[] {});
 		assertEquals(0, actual.ConvertToDecimalInt());
 	}
+	@Test
+	public void convertToDecimalInt_enterNotEmptyString_result43() {
+		BinaryArray actual = new BinaryArray(new int[] {1,0,1,0,1,1});
+		assertEquals(43, actual.ConvertToDecimalInt());
+	}
+	
 }
