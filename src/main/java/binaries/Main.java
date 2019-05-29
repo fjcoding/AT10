1ece6a9a59dfc62652e9a802ae2bc5db3cdc810c
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-29 17:37:54 -0400

    Add required changes to Main.java

--- src/main/java/binaries/Main.java
+++ src/main/java/binaries/Main.java
@@ -26,10 +26,14 @@ public class Main {
     static final String MADAY = "maday";
     static final String MELISSA = "melissa";
 
+    static final String B = "b";
+    static final String H = "h";
+    
     public static void main(String[] args) {
         String student = getStudent(args);
         int[] argsAsInts = toInts(args);
-        printResult(student, argsAsInts);
+        String baseArg = getBase(args);
+        printResult(student, argsAsInts, baseArg);
     }
 
     private static String getStudent(String[] args) {
@@ -41,26 +45,59 @@ public class Main {
     }
 
     private static int[] toInts(String[] args) {
-        int[] argsAsInts = new int[args.length];
-        for (int index = BINARY_START; index < args.length; index++) {
-            argsAsInts[index] = Integer.parseInt(args[index]);
+        int[] argsAsInts = new int[args.length - 2];
+        for (int index = BINARY_START; index < (args.length - 1); index++) {
+            
+            if ((args[index].charAt(0) >= 'A') &&  ((args[index].charAt(0) <= 'F'))) {
+                argsAsInts[index - 1] = (args[index].charAt(0)) - 55;
+            }
+            else if ((args[index].charAt(0) >= 'a') &&  ((args[index].charAt(0) <= 'f'))) {
+                argsAsInts[index - 1] = (args[index].charAt(0)) - 87;
+            }
+            else if ((args[index].charAt(0) >= '0') &&  ((args[index].charAt(0) <= '9'))) {
+                argsAsInts[index - 1] = Integer.parseInt(args[index]);
+            }
+            else {
+                throw new IllegalArgumentException("Invalid digits entered!");
+            }
         }
         return argsAsInts;
     }
 
-    private static void printResult(String student, int[] argsAsInts) {
+    private static String getBase(String[] args) {
+        String baseArg = args[args.length - 1];
+        return baseArg;
+    }
+    
+    private static void printResult(String student, int[] argsAsInts, String baseArg) {
         int result = 0;
-        switch (student) {
-            case ALEJANDRO: result = new binaries.alejandro.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case ANDRES: result = new binaries.andres.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case JESUS: result = new binaries.jesus.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case JOHN: result = new binaries.john.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case JOSUE: result = new binaries.josue.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case LIMBERT: result = new binaries.limbert.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case MADAY: result = new binaries.maday.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            case MELISSA: result = new binaries.melissa.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
-            default: throw new IllegalArgumentException("Unknown student! who are you?");
+        switch (baseArg) {
+            case B:
+                switch (student) {
+                    case ALEJANDRO: result = new binaries.alejandro.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case ANDRES: result = new binaries.andres.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case JESUS: result = new binaries.jesus.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case JOHN: result = new binaries.john.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case JOSUE: result = new binaries.josue.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case LIMBERT: result = new binaries.limbert.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case MADAY: result = new binaries.maday.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    case MELISSA: result = new binaries.melissa.BinaryArray(argsAsInts).ConvertToDecimalInt(); break;
+                    default: throw new IllegalArgumentException("Unknown student! who exactly are you?");
+                }
+                System.out.println(student + "'s result is: " + result);
+                break;
+            
+            case H:
+                switch (student) {
+                    case ALEJANDRO: result = new binaries.alejandro.HexArray(argsAsInts).ConvertToDecimalInt(); break;
+                    default: throw new IllegalArgumentException("Unknown student! who exactly are you?");
+                }
+                System.out.println(student + "'s result is: " + result);
+                break;
+            
+            default: 
+                throw new IllegalArgumentException("Invalid or missing base argument");
+
         }
-        System.out.println(student + "'s result is: " + result);
     }
 }
