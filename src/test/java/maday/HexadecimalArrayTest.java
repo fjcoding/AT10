f2493adeb8b206675f36c4bc619288ec026bcc40
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-29 17:34:41 -0400

    Basic unit test (empty) added

--- src/test/java/maday/HexadecimalArrayTest.java
+++ src/test/java/maday/HexadecimalArrayTest.java
@@ -5,5 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HexadecimalArrayTest {
+    HexadecimalArray hexadecimalArray;
+
+    @Test
+    public void ConvertToDecimalInt_EmptyArray_Result0() {
+        hexadecimalArray = new HexadecimalArray(new int[]{});
+        int expected = 0;
+        int actual = hexadecimalArray.ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
 
 }
