24ba698fc2aec8d79b12ec150893b2cee6f8ce49
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-29 11:54:22 -0400

    Basic unit test added

--- src/test/java/maday/BinaryArrayTest.java
+++ src/test/java/maday/BinaryArrayTest.java
@@ -5,4 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+    BinaryArray binaryArray;
+
+    @Test
+    public void ConvertToDecimalInt_EmptyArray_Result() {
+        binaryArray = new BinaryArray(new int[]{});
+        int expected = 0;
+        int actual = binaryArray.ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
 }
