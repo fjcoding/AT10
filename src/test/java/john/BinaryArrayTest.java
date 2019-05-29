2a6f5c851d2ac48cffc49aa168165731fd61fba0
John Salazar Pinto <JohnSalazar@fundacion-jala.local>
2019-05-29 17:51:20 -0400

    Test implementation

--- src/test/java/john/BinaryArrayTest.java
+++ src/test/java/john/BinaryArrayTest.java
@@ -5,4 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+    @Test
+    public void Binary_unitTest2() {
+        int expected = 2;
+        BinaryArray binary = new BinaryArray(new int[]{1, 0});
+        int actual = binary.ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
 }
