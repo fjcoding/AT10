5de87db56bb6e892de31f68e1bc619bef0793632
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-29 17:29:56 -0400

    Create the classes HexadecimalArray and HexadecimalArrayTest.

---
+++ src/main/java/binaries/maday/HexadecimalArray.java
@@ -0,0 +1,23 @@
+package binaries.maday;
+
+public class HexadecimalArray {
+    /**
+     * Array of hexadecimal numbers expected
+     */
+    private int[] hexadecimal;
+
+    /**
+     * Simple constructor that assigns hexadecimal list to instace's state
+     */
+    public HexadecimalArray(int[] hexadecimal) {
+        this.hexadecimal = hexadecimal;
+    }
+
+    /**
+     * Returns the decimal numeric value equivalent to the list of hexadecimal stored
+     */
+    public int ConvertToDecimalInt() {
+        int decimal;
+        return -1;
+    }
+}
---
+++ src/test/java/maday/HexadecimalArrayTest.java
@@ -0,0 +1,9 @@
+package binaries.maday;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HexadecimalArrayTest {
+
+}
