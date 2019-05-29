622d5633e12c6ff7381fc9df579df7ac3606af31
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-05-29 11:44:20 -0400

    Exception and Unit test implementation.

--- src/main/java/binaries/melissa/BinaryArray.java
+++ src/main/java/binaries/melissa/BinaryArray.java
@@ -17,6 +17,21 @@ public class BinaryArray {
     }
 
     /**
+     * Verifies if BinaryArray corresponds to an actual binary number.
+     * 
+     * @return
+     */
+    public boolean isValid() {
+        boolean answer = true;
+        for (int index = 0; index < binaries.length; index++) {
+            if (binaries[index] != 0 && binaries[index] != 1) {
+                answer = false;
+            }
+        }
+        return answer;
+    }
+
+    /**
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
     public int ConvertToDecimalInt() {
@@ -28,6 +43,27 @@ public class BinaryArray {
             decimal = decimal + binaries[index] * (int) powOfTwo;
         }
         return decimal;
+    }
+
+    /**
+     * Returns the decimal numeric value equivalent to the list of binaries stored
+     * if the input is an actual binary number, in case it is not throws an
+     * Exception message     
+     * @return
+     */
+    public int ConvertToDecimalIntInoutValidation() {
+        if (isValid()) {
+            int decimal = 0;
+            final int BASE_NUMBER = 2;
+            double powOfTwo;
+            for (int index = 0; index < binaries.length; index++) {
+                powOfTwo = Math.pow(BASE_NUMBER, (binaries.length - 1) - index);
+                decimal = decimal + binaries[index] * (int) powOfTwo;
+            }
+            return decimal;
+        } else {
+            throw new UnsupportedOperationException("Invalid input");
+        }
     }
 
 }
--- src/test/java/melissa/BinaryArrayTest.java
+++ src/test/java/melissa/BinaryArrayTest.java
@@ -5,4 +5,52 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+
+    @Test
+    public void ConvertToDecimalInt_validInPut43_correctResult() {
+        int[] array = { 1, 0, 1, 0, 1, 1 };
+        BinaryArray binary = new BinaryArray(array);
+        assertEquals(43, binary.ConvertToDecimalInt());
+    }
+
+    @Test
+    public void ConvertToDecimalInt_validInPut345_correctResult() {
+        int[] array = { 1, 0, 1, 0, 1, 1, 0, 0, 1 };
+        BinaryArray binary = new BinaryArray(array);
+        assertEquals(345, binary.ConvertToDecimalInt());
+    }
+
+    @Test
+    public void isValid_validInPut_correctResult() {
+        int[] array = { 1, 0, 1, 1 };
+        BinaryArray binary = new BinaryArray(array);
+        assertTrue(binary.isValid());
+    }
+
+    @Test
+    public void isValid_InvalidInPut_correctResult() {
+        int[] array = { 1, 0, 1, 2 };
+        BinaryArray binary = new BinaryArray(array);
+        assertFalse(binary.isValid());
+    }
+
+    @Test(expected = Exception.class)
+    public void ConvertToDecimalInt_invalidInPutShouldThrowException() {
+        int[] array = { 1, 0, 1, 2 };
+        BinaryArray binary = new BinaryArray(array);
+        binary.ConvertToDecimalIntInoutValidation();
+    }
+
+    @Test
+    public void ConvertToDecimalInt_invalidInputShouldThrowException_correctMessage() {
+        try {
+            int[] array = { 1, 0, 1, 0, 1, 1, 3, 0, 1 };
+            BinaryArray binary = new BinaryArray(array);
+            binary.ConvertToDecimalIntInoutValidation();
+        } catch (Exception e) {
+            String expected = "Invalid input";
+            String actual = e.getMessage();
+            assertEquals(expected, actual);
+        }
+    }
 }
