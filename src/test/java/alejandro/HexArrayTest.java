d6be4b76c08ea4814e87a8fb61521db376044bfe
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-29 17:34:05 -0400

    Add cases to HexArrayTest

--- src/test/java/alejandro/HexArrayTest.java
+++ src/test/java/alejandro/HexArrayTest.java
@@ -5,10 +5,43 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HexArrayTest {
+    @Test (expected = IllegalArgumentException.class)
+    public void ConvertToDecimalInt_noDigits_resultIllegalArgumentException() {
+        (new HexArray(new int[]{})).ConvertToDecimalInt();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void ConvertToDecimalInt_invalidDigits_resultIllegalArgumentException() {
+        (new HexArray(new int[]{-1, 36, -9})).ConvertToDecimalInt();
+    }
+    
+
+    @Test
+    public void ConvertToDecimalInt_singleBit0_result0() {
+        int expected = 0;
+        int actual = (new HexArray(new int[]{0})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void ConvertToDecimalInt_singleBit1_result15() {
         int expected = 15;
         int actual = (new HexArray(new int[]{15})).ConvertToDecimalInt();
         assertEquals(expected, actual);
     }
+    
+    @Test
+    public void ConvertToDecimalInt_mostSignificativeBit0_result509() {
+        int expected = 4353;
+        int actual = (new HexArray(new int[]{0, 1, 1, 0, 1})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalInt_mostSignificativeBit1_result509() {
+        int expected = 4353;
+        int actual = (new HexArray(new int[]{1, 1, 0, 1})).ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
+    
 }
