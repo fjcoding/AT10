7a528b89ae6efe9c86282ed600491f881bca429b
Maday <Maday.Alcala@fundacion-jala.org>
2019-05-31 14:17:37 -0400

    Unit test with different values added

--- src/test/java/maday/HexadecimalArrayTest.java
+++ src/test/java/maday/HexadecimalArrayTest.java
@@ -14,4 +14,12 @@ public class HexadecimalArrayTest {
         int actual = hexadecimalArray.ConvertToDecimalInt();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void ConvertToDecimalInt_NotEmptyArray_Result109519() {
+        hexadecimalArray = new HexadecimalArray(new String[]{"1", "A", "B", "C", "F"});
+        int expected = 109519;
+        int actual = hexadecimalArray.ConvertToDecimalInt();
+        assertEquals(expected, actual);
+    }
 }
