d228cb7c7abb42475e0caba8066303c3acd5aa7b
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-31 14:47:33 -0400

    Implement ConvertToDecimalInt method
    Implement ConvertToDecimalInt in BinaryArray class
    Unit Test Pass OK

--- src/main/java/binaries/alejandro/BinaryArray.java
+++ src/main/java/binaries/alejandro/BinaryArray.java
@@ -17,7 +17,45 @@ public class BinaryArray {
     /**
      * Returns the decimal numeric value equivalent to the list of binaries stored
      */
-    public int ConvertToDecimalInt() {
-        return 0;
+    public int ConvertToDecimalInt(char baseArg) {
+        int sourceBase;
+        int result = 0;
+
+        if (binaries.length != 0) {
+            switch (baseArg) {
+                case 'b':
+                    sourceBase = 2;
+                    break;
+            
+                case 'h':
+                    sourceBase = 16;
+                    break;
+            
+                default:
+                    throw new IllegalArgumentException("Invalid base argument or not supported yet!");
+            }
+
+            int upperLimit = binaries.length - 1;
+            int lowerLimit = 0;
+            String stringDigit = "";
+            for (int arrayIndex = upperLimit; arrayIndex >= lowerLimit; arrayIndex--) {
+                stringDigit = binaries[arrayIndex];
+                if (stringDigit.length() == 1) {
+                    try {
+                    result += (Integer.parseInt(stringDigit, sourceBase)) * (Math.pow(sourceBase, upperLimit - arrayIndex));
+                    }
+                    catch (NumberFormatException e) {
+                        throw new NumberFormatException("Invalid digits or not supported!");
+                    }
+                }
+                else {
+                    throw new NumberFormatException("Enter single positive digits only");
+                }
+            }
+            return result;
+        }
+        else {
+            throw new IllegalArgumentException("Caution: No digits were entered!");
+        }
     }
 }
