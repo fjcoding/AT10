6f4e27a4b43b9c03f1c9c5bfce4765f593c64c2d
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-31 14:59:34 -0400

    Add Unit Test Cases
    * Add unit test cases for hexadecimal digits input
    * Tests pass OK

--- src/test/java/alejandro/BinaryArrayTest.java
+++ src/test/java/alejandro/BinaryArrayTest.java
@@ -5,13 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
-    @Test (expected = IllegalArgumentException.class)
+    /********************************/
+    /* Test for binary digits input */
+    /********************************/
+    @Test (expected = Exception.class)
     public void ConvertToDecimalIntB_noDigits_resultIllegalArgumentException() {
         (new BinaryArray(new String[]{})).ConvertToDecimalInt('b');
     }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void ConvertToDecimalIntB_outOfBandDigitsMinus9_resultIllegalArgumentException() {
+    @Test (expected = Exception.class)
+    public void ConvertToDecimalIntB_outOfBandDigitsMinus9_resultNumberFormatException() {
         (new BinaryArray(new String[]{"-9"})).ConvertToDecimalInt('b');
     }
 
@@ -45,6 +48,52 @@ public class BinaryArrayTest {
     public void ConvertToDecimalIntB_mostSignificativeBit1_result509() {
         int expected = 509;
         int actual = (new BinaryArray(new String[]{"1", "1", "1", "1", "1", "1", "1", "0", "1"})).ConvertToDecimalInt('b');
+        assertEquals(expected, actual);
+    }
+
+    /*****************************/
+    /* Test for hex digits input */
+    /*****************************/
+    @Test (expected = Exception.class)
+    public void ConvertToDecimalIntH_noDigits_resultIllegalArgumentException() {
+        (new BinaryArray(new String[]{})).ConvertToDecimalInt('h');
+    }
+
+    @Test (expected = Exception.class)
+    public void ConvertToDecimalIntH_outOfBoundsDigitsMinusB_resultNumberFormatException() {
+        (new BinaryArray(new String[]{"-B"})).ConvertToDecimalInt('h');
+    }
+
+    @Test (expected = Exception.class)
+    public void ConvertToDecimalIntH_outOfBoundsDigitsH_resultNumberFormatException() {
+        (new BinaryArray(new String[]{"H"})).ConvertToDecimalInt('h');
+    }
+
+    @Test
+    public void ConvertToDecimalIntH_singleBit0_result0() {
+        int expected = 0;
+        int actual = (new BinaryArray(new String[]{"0"})).ConvertToDecimalInt('h');
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalIntH_singleBit1_result1() {
+        int expected = 1;
+        int actual = (new BinaryArray(new String[]{"1"})).ConvertToDecimalInt('h');
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalIntH_mostSignificativeBit0_result69889() {
+        int expected = 69889;
+        int actual = (new BinaryArray(new String[]{"0", "1", "1", "1", "0", "1"})).ConvertToDecimalInt('h');
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ConvertToDecimalIntH_mostSignificativeBit1_result69889() {
+        int expected = 69889;
+        int actual = (new BinaryArray(new String[]{"1", "1", "1", "0", "1"})).ConvertToDecimalInt('h');
         assertEquals(expected, actual);
     }
 }
