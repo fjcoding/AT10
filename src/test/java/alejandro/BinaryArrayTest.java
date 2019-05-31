8f264e81270082d1224988fc573dca0739753b42
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-31 14:50:40 -0400

    Add Unit Test Case
    Unit test for checking single 0 digit
    Unit test passes OK

--- src/test/java/alejandro/BinaryArrayTest.java
+++ src/test/java/alejandro/BinaryArrayTest.java
@@ -6,6 +6,13 @@ import org.junit.Test;
 
 public class BinaryArrayTest {
     @Test
+    public void ConvertToDecimalIntB_singleBit0_result0() {
+        int expected = 0;
+        int actual = (new BinaryArray(new String[]{"0"})).ConvertToDecimalInt('b');
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void ConvertToDecimalIntB_singleBit1_result1() {
         int expected = 1;
         int actual = (new BinaryArray(new String[]{"1"})).ConvertToDecimalInt('b');
