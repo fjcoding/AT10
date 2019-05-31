448849319b9201a0ac960851c41d4bead7f61347
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-31 17:48:52 -0400

    unit-test-hexadecimal

--- src/main/java/binaries/josue/BinaryArray.java
+++ src/main/java/binaries/josue/BinaryArray.java
@@ -1,27 +1,49 @@
 package binaries.josue;
 
 public class BinaryArray {
-    final int base = 2;
+    final int BINARIEBASE = 2;
+    final int HEXABASE = 16;
     /**
      * Array of 1s and 0s expected
      */
-    private int[] binaries;
+    private String[] binaries;
+    private char base;
     /**
      * Simple constructor that assigns binaries list to instace's state
      */
-    public BinaryArray(int[] binaries) {
+    public BinaryArray(String[] binaries, char base) {
         this.binaries = binaries;
+        this.base=base;
     }
-
     /**
      * Returns the decimal numeric value equivalent to the list of binaries
      * stored
      */
     public int ConvertToDecimalInt() {
-        int result = 0;
+    	int result = 0;
+    	if(base == 'b') {
+    		result = binariesToDecimal();
+    	}
+        if(base == 'h') {
+        	result = hexadecimalToDecimal();
+        }
+        
+        return result;
+    }
+    public int binariesToDecimal() {
+    	int result = 0;
         int cont = 0;
-        for (int index = binaries.length - 1; index > 0; index--) {
-            result = result + ((int) (Math.pow(base, cont)) * binaries[index]);
+        for (int index = binaries.length - 1; index >= 0; index--) {
+            result = result + ((int) (Math.pow(BINARIEBASE, cont)) * Integer.parseInt(binaries[index]));
+            cont++;
+        }
+        return result;
+    }
+    public int hexadecimalToDecimal() {
+    	int result = 0;
+        int cont = 0;
+        for (int index = binaries.length - 1; index >= 0; index--) {
+            result = result + ((int) (Math.pow(HEXABASE, cont)) * Integer.parseInt(binaries[index],16));
             cont++;
         }
         return result;
--- src/test/java/josue/BinaryArrayTest.java
+++ src/test/java/josue/BinaryArrayTest.java
@@ -5,4 +5,52 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BinaryArrayTest {
+	@Test
+	public void binariesToDecimal_withArrayEmpty_resultTrue() {
+		BinaryArray actual = new BinaryArray(new String[] {},'b');
+		int expected = 0;
+		assertEquals(expected,actual.binariesToDecimal());
+	}
+	@Test
+	public void binariesToDecimal_withArrayEmpty_resultFalse() {
+		BinaryArray actual = new BinaryArray(new String[] {"1","0"},'b');
+		int expected = 0;
+		assertNotEquals(expected,actual.binariesToDecimal());
+	}
+	@Test
+	public void binariesToDecimal_withArrayNotEmpty_resultTrue() {
+		BinaryArray actual = new BinaryArray(new String[] {"1","0","1","0","1","1"},'b');
+		int expected = 43;
+		assertEquals(expected,actual.binariesToDecimal());
+	}
+	@Test
+	public void binariesToDecimal_withArrayNotEmpty_resultFalse() {
+		BinaryArray actual = new BinaryArray(new String[] {"1","0"},'b');
+		int expected = 43;
+		assertNotEquals(expected,actual.binariesToDecimal());
+	}
+	@Test
+	public void hexaToDecimal_withArrayEmpty_resultTrue() {
+		BinaryArray actual = new BinaryArray(new String[] {},'h');
+		int expected = 0;
+		assertEquals(expected,actual.hexadecimalToDecimal());
+	}
+	@Test
+	public void hexaToDecimal_withArrayEmpty_resultFalse() {
+		BinaryArray actual = new BinaryArray(new String[] {"1"},'h');
+		int expected = 0;
+		assertNotEquals(expected,actual.hexadecimalToDecimal());
+	}
+	@Test
+	public void hexaToDecimal_withArrayNotEmpty_resultTrue() {
+		BinaryArray actual = new BinaryArray(new String[] {"a"},'h');
+		int expected = 10;
+		assertEquals(expected,actual.hexadecimalToDecimal());
+	}
+	@Test
+	public void hexaToDecimal_withArrayNotEmpty_resultFalse() {
+		BinaryArray actual = new BinaryArray(new String[] {"a"},'h');
+		int expected = 11;
+		assertNotEquals(expected,actual.hexadecimalToDecimal());
+	}
 }
