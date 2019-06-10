afad7f7f096507891f9a561122e64a36879ad577
Limbert Vargas Laura <LimbertVargas@Fundacion-jala.local>
2019-06-10 19:21:46 -0400

    Modified AccountNumber class adding digit matrix

--- src/test/java/limbert/AccountNumberTest.java
+++ src/test/java/limbert/AccountNumberTest.java
@@ -7,8 +7,16 @@ public class AccountNumberTest {
     @Test
     public void getDigit_six_sixMatrix() {
         AccountNumber accountNumber = new AccountNumber();
-        int digit = 4;       
-        assertEquals(digit,accountNumber.getDigit(3));
+        int digit = 6;
+        assertEquals(digit, accountNumber.getDigit(5));
+    }
+
+    @Test
+    public void setDigit_digitThree_return() {
+        AccountNumber accountNumber = new AccountNumber();
+        char matrixThree[][]=new char[][] {' ','_',' ' },{' ','_','|'},{' ','_','|'},{' ',' ',' '}
+        int expected = 0;
+        AccountNumber.setDigit(expected,matrixThree);
     }
 
 }
