6c67f85833f72c10b82e59e80a6d43306779427f
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-06-12 13:57:31 -0400

    Add Digit class unit tests

--- src/test/java/BankOCR/DigitTest.java
+++ src/test/java/BankOCR/DigitTest.java
@@ -28,4 +28,60 @@ public class DigitTest {
         int actual = digit.convertArrays();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void convertArrays_CorrectResult3() {
+        int expected = 3;
+        Digit digit = new Digit(Digit.THREE);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertArrays_CorrectResult4() {
+        int expected = 4;
+        Digit digit = new Digit(Digit.FOUR);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertArrays_CorrectResult5() {
+        int expected = 5;
+        Digit digit = new Digit(Digit.FIVE);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertArrays_CorrectResult6() {
+        int expected = 6;
+        Digit digit = new Digit(Digit.SIX);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertArrays_CorrectResult7() {
+        int expected = 7;
+        Digit digit = new Digit(Digit.SEVEN);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertArrays_CorrectResult8() {
+        int expected = 8;
+        Digit digit = new Digit(Digit.EIGHT);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertArrays_CorrectResult9() {
+        int expected = 9;
+        Digit digit = new Digit(Digit.NINE);
+        int actual = digit.convertArrays();
+        assertEquals(expected, actual);
+    }
 }
