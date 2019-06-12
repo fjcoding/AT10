b2ad54c0fde2f13cfb4848a94d7fc545edcc588a
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-06-12 19:10:45 -0400

    validateAccount method implemented
    getAccountToString method added to support leading zeros

--- src/main/java/KataBankOCR/AccountNumber.java
+++ src/main/java/KataBankOCR/AccountNumber.java
@@ -1,21 +1,50 @@
 package KataBankOCR;
 
 public class AccountNumber {
-	String head_number,body_number,foot_number;
-	Integer accountNumber = 0;
-	public AccountNumber(String head_number,String body_number,String foot_number) {
-		this.head_number=head_number;
-		this.body_number=body_number;
-		this.foot_number=foot_number;
-		
-	}
-	public Integer getAccountNumber() {
-		 
-		for(int index = 0; index<head_number.length();index+=3) {
-			String actualNumber = head_number.substring(index, index+3)+body_number.substring(index, index+3)+foot_number.substring(index, index+3);
-			Digit digit = new Digit(actualNumber);
-			accountNumber = accountNumber*10+digit.getDigit();
-		}
-		return accountNumber;
-	}
+    String head_number, body_number, foot_number;
+    Integer accountNumber = 0;
+
+    public AccountNumber(String head_number, String body_number, String foot_number) {
+        this.head_number = head_number;
+        this.body_number = body_number;
+        this.foot_number = foot_number;
+
+    }
+
+    public Integer getAccountNumber() {
+
+        for (int index = 0; index < head_number.length(); index += 3) {
+            String actualNumber = head_number.substring(index, index + 3) + body_number.substring(index, index + 3)
+                    + foot_number.substring(index, index + 3);
+            Digit digit = new Digit(actualNumber);
+            accountNumber = accountNumber * 10 + digit.getDigit();
+        }
+        return accountNumber;
+    }
+
+    public String getAccountNumberToString() {
+        Integer VALIDLENGTH = 9;
+        String finalNumber = "";
+        String convertedNumber = getAccountNumber().toString();
+        for (int index = 0; index <= VALIDLENGTH - convertedNumber.length() - 1; index++) {
+            finalNumber += "0";
+        }
+        finalNumber += convertedNumber;
+        return finalNumber;
+    }
+
+    public Boolean validateAccountNumber() {
+        Boolean answer = false;
+        String convertNumber = getAccountNumberToString();
+        Integer VALIDLENGTH = 9;
+        Integer sum = 0;
+        for (int index = 0; index < VALIDLENGTH; index++) {
+            Character digitCharacter = convertNumber.charAt(index);
+            sum += Character.getNumericValue(digitCharacter) * (VALIDLENGTH - index);
+        }
+        if (sum % 11 == 0) {
+            answer = true;
+        }
+        return answer;
+    }
 }
--- src/test/java/KataBankOCR/AccountNumberTest.java
+++ src/test/java/KataBankOCR/AccountNumberTest.java
@@ -14,4 +14,32 @@ public class AccountNumberTest {
 		Integer expected = 123456789;
 		assertEquals(expected,accountNumber.getAccountNumber());
 	}
+	
+	@Test
+    public void accountNumberToString_correctResult() { 
+        String head_number = " _  _  _     _  _  _  _  _ ";
+        String body_number = "| | _| _||_||_ |_   ||_|| |";
+        String foot_number = "|_||_  _|  | _||_|  ||_||_|";
+        AccountNumber accountNumber = new AccountNumber(head_number,body_number,foot_number);
+        String expected = "023456780";
+        assertEquals(expected,accountNumber.getAccountNumberToString());
+    }
+	
+	@Test
+    public void validateAccountNumber_correctResult() { 
+        String head_number = " _     _  _  _  _  _  _  _ ";
+        String body_number = " _||_||_ |_||_| _||_||_ |_ ";
+        String foot_number = " _|  | _||_||_||_ |_||_| _|";
+        AccountNumber accountNumber = new AccountNumber(head_number,body_number,foot_number);
+        assertTrue(accountNumber.validateAccountNumber());
+    }
+    
+    @Test
+    public void validateAccountNumber_incorrectResult() { 
+        String head_number = " _  _     _  _        _  _ ";
+        String body_number = "|_ |_ |_| _|  |  ||_||_||_ ";
+        String foot_number = "|_||_|  | _|  |  |  | _| _|";
+        AccountNumber accountNumber = new AccountNumber(head_number,body_number,foot_number);
+        assertFalse(accountNumber.validateAccountNumber());
+    }
 }
