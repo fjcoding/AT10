bb8457ef81f195b3b52e07eb79f5a6da077c5dfc
John Salazar Pinto <JohnSalazar@fundacion-jala.local>
2019-06-17 19:16:08 -0400

    Matrix and unit test implementation

---
+++ src/main/java/Minesweeper/Minesweeper.java
@@ -0,0 +1,15 @@
+package binaries.Minesweeper;
+import java.util.Arrays;
+
+
+public class Minesweeper {
+        
+    public Minesweeper(char[][] matrix) {
+        
+    }
+        
+    public char[][] matrixGenerator() {
+        char[][] matrix = {{'0', '0'}, {'0', '0'}};
+        return matrix;
+    }
+}
---
+++ src/test/java/Minesweeper/MinesweeperTest.java
@@ -0,0 +1,15 @@
+package Minesweeper;
+import java.util.Arrays;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinesweeperTest{
+    //Matrix implementation
+    @Test
+    public void matrix() {
+        char[][] matrix = new char[2][2];
+        matrix[0] = new char[] {'.', '.'};
+        matrix[1] = new char[] {'.', '.'};
+    }
+}
