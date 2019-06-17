2e0b93dfa09c5af317f06a8a12c75e2213ddbcbe
Jesus.Menacho@fundacion-jala.org <Jesus.Menacho@fundacion-jala.org>
2019-06-17 19:02:32 -0400

    implement methot llenar

--- src/main/java/Minesweeper/Board.java
+++ src/main/java/Minesweeper/Board.java
@@ -1,10 +1,20 @@
 package Minesweeper;
-
 import java.util.Scanner;
-
 public class Board {
+    String tablero[][] = new String[100][100];
+    int n;
+    int m;
     public boolean asignar(int n, int m){
-        int tablero[][] = new int[n][m];
+
+        this.n=n;
+        this.m=m;
+
         return true;
+    }
+    public boolean llenar (){
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < m; i++)
+                tablero[j][i] = "*";
+            return true;
     }
 }
--- src/main/java/Minesweeper/BoardTest.java
+++ src/main/java/Minesweeper/BoardTest.java
@@ -7,10 +7,14 @@ import static org.junit.Assert.assertEquals;
 public class BoardTest {
     Board board=new Board();
     @Test
-    public void addValuer_Board_Result() throws Exception {
+    public void addValueCreateBoard_ResultTrue() throws Exception {
         Boolean expected = true;
         int n=4;
         int m=4;
         assertEquals(expected, board.asignar(n,m));
+    }
+    public void addValuefillWithAsterisco_True() throws Exception {
+        Boolean expected = true;
+        assertEquals(expected, board.llenar());
     }
 }
