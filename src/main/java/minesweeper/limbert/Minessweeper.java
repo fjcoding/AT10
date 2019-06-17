9f54484fc1b22f12446717f40f6aa761cf8ca26e
Limbert Vargas Laura <LimbertVargas@Fundacion-jala.local>
2019-06-17 19:18:03 -0400

    implement Board class on Minesweeper and MinesweeperTest classes

---
+++ src/main/java/minesweeper/limbert/Minessweeper.java
@@ -0,0 +1,25 @@
+package minesweeper.limbert;
+
+public class Minessweeper {
+    public class Board {
+
+      
+        public Board(String coordinates) {
+            
+        }
+        
+        public boolean reveal(int positionX, int positionY) {
+            return false;
+        }
+
+        public boolean isRevealed(int positionX, int positionY) {
+            return false;
+        }
+
+        public int getNumberOfSurroundingMines(int positionX, int positionY) {
+            return 0;
+        }
+    }
+}
+
+
