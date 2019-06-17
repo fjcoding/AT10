4f3f16403f36fbc0fa24de2af33ffcb297124a8e
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-06-17 19:26:12 -0400

    correctValues() implemented method

--- src/main/java/minesweeper/Head.java
+++ src/main/java/minesweeper/Head.java
@@ -1,21 +1,31 @@
 package minesweeper;
 
 public class Head {
-	String head;
-	String lines;
-	String columns;
-	String [] linesColumns;
-	public Head(String head) {
-		this.head = head;
-	}
-	public String[] linesColumns() {
-		linesColumns = head.split(" ");
-		return linesColumns;
-	}
-	public String lines() {
-		return linesColumns[0];
-	}
-	public String columns() {
-		return linesColumns[1];
-	}
+    String head;
+    String lines;
+    String columns;
+    String[] linesColumns;
+    public Head(String head) {
+        this.head = head;
+    }
+    public String[] linesColumns() {
+        linesColumns = head.split(" ");
+        return linesColumns;
+    }
+    public String lines() {
+        return linesColumns[0];
+    }
+    public String columns() {
+        return linesColumns[1];
+    }
+    public boolean correctValues() {
+        boolean result = false;
+        if (Integer.parseInt(lines) > 0 && Integer.parseInt(lines) < 100) {
+            if (Integer.parseInt(columns) > 0
+                    && Integer.parseInt(columns) < 100) {
+                result = true;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/minesweeper/HeadTest.java
+++ src/test/java/minesweeper/HeadTest.java
@@ -6,18 +6,24 @@ import org.junit.Test;
 
 public class HeadTest {
 
-	@Test
-	public void lines_23_result2() {
-		Head head = new Head("2 3");
-		String excepted = "2";
-		head.linesColumns();
-		assertEquals(excepted, head.lines());
-	}
-	@Test
-	public void columns_23_result3() {
-		Head head = new Head("2 3");
-		String excepted = "3";
-		head.linesColumns();
-		assertEquals(excepted, head.columns());
-	}
+    @Test
+    public void lines_2and3_result2() {
+        Head head = new Head("2 3");
+        String excepted = "2";
+        head.linesColumns();
+        assertEquals(excepted, head.lines());
+    }
+    @Test
+    public void columns_2and3_result3() {
+        Head head = new Head("2 3");
+        String excepted = "3";
+        head.linesColumns();
+        assertEquals(excepted, head.columns());
+    }
+    @Test
+    public void correctValues_negative1and5_resultTrue() {
+        Head head = new Head("-1 3");
+        head.linesColumns();
+        assertTrue(head.correctValues());
+    }
 }
